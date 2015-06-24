6cfa1bb49d56bb6f7e6620bbe76f08aeea9ab54e
rizamasta <rizamasta@gmail.com>
2015-06-24 13:54:55 +0700

    update module user

--- application/modules/user/controller/indexcontroller.php
+++ application/modules/user/controller/indexcontroller.php
@@ -65,13 +65,15 @@ class Indexcontroller extends Controller {
             $this->redirect('error');
         }
         $where = $this->_modelUser->getAdapterSelect()->quoteInto('id_user = ?', $id);
-        $data = $this->_modelUser->fetchRow($where)->toArray()
-        ;
+        $data = $this->_modelUser->fetchRow($where)->toArray();
         if (empty($data)) {
-            $this->redirect();
+            $this->redirect('error');
         }
         $data;
         pr($data);
+//        require_once UD . 'header.html';
+//        include APP_MODUL . '/user/form/form-changepassword.phtml';
+//        require_once UD . 'footer.html';
     }
     
     public function delete($id) {
@@ -88,8 +90,8 @@ class Indexcontroller extends Controller {
             $this->redirect('error');
         }
         require_once UD . 'header.html';
-//        $where = $this->_modelUser->getAdapter()->quoteInto('id_user = ?', $id);
-//        $data = $this->_modelUser->fetchRow($where)->toArray();
+        $where = $this->_modelUser->getAdapter()->quoteInto('id_user = ?', $id);
+        $data = $this->_modelUser->fetchRow($where)->toArray();
         $data = $this->_modelUser->profile($id);
         include APP_MODUL . '/user/form/form-profile.phtml';
         require_once UD . 'footer.html';
@@ -98,6 +100,41 @@ class Indexcontroller extends Controller {
     }
     
     public function changepassword(){
-        echo 'leo';
+        require_once UD . 'header.html';
+        include APP_MODUL . '/user/form/form-changepassword.phtml';
+        require_once UD . 'footer.html';
     }
+
+    public function savenewpassword(){
+        $form   = $this->getPost();
+        $where  = $this->_modelUser->getAdapterSelect()->quoteInto('id_user = ?', $_SESSION['dataLogin']['id_user']);
+        $data   = $this->_modelUser->fetchRow($where)->toArray();
+        $msg    = Array();
+
+        sha1($form['oldPassword'])!=$data['password']?$msg['password']="Password Lama Salah!":$ms="";
+        $form['newPassword']!=$form['newPassword2']?$msg['newPassword']="Password baru tidak cocok!":$ms="";
+
+
+        if(empty($msg)){
+            $setData['password']=sha1($form['newPassword2']);
+            $where = $this->_modelUser->getAdapter()->quoteInto('id_user = ?', $data['id_user']);
+            try {
+                $this->_modelUser->update($setData, $where);
+                $this->redirect('user/index/profile/'.$data['id_user']);
+            } catch (Exception $ex) {
+                require UD . 'header.html';
+                $msg = $ex->getMessage();
+                require APP_MODUL .'/user/form/form-changepassword.phtml';
+                require UD . 'footer.html';
+            }
+        }
+        else{
+            require_once UD . 'header.html';
+            include APP_MODUL . '/user/form/form-changepassword.phtml';
+            require_once UD . 'footer.html';
+        }
+
+
+    }
+
 }
