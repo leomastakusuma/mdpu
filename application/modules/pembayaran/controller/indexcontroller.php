deb04fe0858b8590167690bd0a3ad78c4c8cd4e0
leomastakusuma <leomastakusuma@gmail.com>
2015-07-22 11:30:55 +0700

    Update Doc

--- application/modules/pembayaran/controller/indexcontroller.php
+++ application/modules/pembayaran/controller/indexcontroller.php
@@ -15,6 +15,8 @@ class IndexController extends Controller {
     protected $_modelSPB;
     protected $_modelCabang;
     protected $_modelUser;
+    protected $_modelPembayaran;
+    protected $_modelKartuPiutang;
     public $id_user;
     public $id_cabang;
 
@@ -26,47 +28,84 @@ class IndexController extends Controller {
         $this->_modelSPB = Mydb::getModelSPB();
         $this->_modelUser = Mydb::getModelUser();
         $this->_modelKendaraan = Mydb::getModelKendaraan();
-        $this->id_user = $_SESSION['dataLogin']['id_user'];
-        $this->id_cabang = $_SESSION['dataLogin']['id_cabang'];
+        $this->_modelPembayaran = Mydb::getModelPembayaran();
+        $this->_modelKartuPiutang = Mydb::getModelKartuPiutang();
+        $this->id_user = $_SESSION[ 'dataLogin' ][ 'id_user' ];
+        $this->id_cabang = $_SESSION[ 'dataLogin' ][ 'id_cabang' ];
     }
 
     public function index() {
-        if (($_SESSION['level'] != 'kasir')) {
-            $this->redirect('error/index/notAllowed');
+        if ( ($_SESSION[ 'level' ] != 'kasir' ) ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
         require UD . 'headerDataTables.phtml';
-        $where = $this->_modelCabang->getAdapter()->quoteInto('id_cabang = ? ', $this->id_cabang);
-        $cabang = $this->_modelCabang->fetchRow($where);
-        if (!$cabang) {
-            $this->redirect('error');
+        $where = $this->_modelCabang->getAdapter()->quoteInto( 'id_cabang = ? ', $this->id_cabang );
+        $cabang = $this->_modelCabang->fetchRow( $where );
+        if ( !$cabang ) {
+            $this->redirect( 'error' );
         }
-        $data = $this->_modelPinjaman->getDataPinjamanByCabang($this->id_user, $this->id_cabang);
-        foreach ($data as $key => $val) {
-            $no_spb = $val['no_kontrak'];
-            $where = $this->_modelSPB->getAdapter()->quoteInto('no_kontrak = ?', $no_spb);
-            $spb = $this->_modelSPB->fetchRow($where);
-            $data[$key]['spb'] = (!empty($spb->no_spb)?$spb->no_spb:'');
-        }
+        $this->id_cabang = $_SESSION[ 'dataLogin' ][ 'id_cabang' ];
+        $data = $this->_modelPembayaran->getDataAnsuran( $this->id_cabang );
+
         require APP_MODUL . '/pembayaran/view/dataPembayaran.phtml';
         require UD . 'footerDataTables.phtml';
     }
 
-    public function add($nopos = null) {
-        if (($_SESSION['level'] != 'superadmin') && ($_SESSION['level'] != 'kasir')) {
-            $this->redirect('error/index/notAllowed');
+    public function add( $nopos = null ) {
+        if ( ($_SESSION[ 'level' ] != 'superadmin') && ($_SESSION[ 'level' ] != 'kasir') ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
 
-        $nik_costumer = $this->_modelCostumer->getNoKontrak();
-        $this->id_user=$_SESSION['dataLogin']['id_user'];
-        $no_polisi = $this->_modelKendaraan->getNoPolisi($this->id_user);
-        if (!empty($nopos)) {
-            $data = $this->_modelKendaraan->getKenCostum($nopos);
-            $where = $this->_modelPinjaman->getAdapter()->quoteInto('nik_costumer =?', $data['nik_costumer']);
-            $cekData = $this->_modelPinjaman->fetchRow($where);
-            $lastid = $this->_modelPinjaman->getLastId();
-            $status = (!empty($cekData) ? '/MD/' : '/MF/');
-            $id_cabang = $_SESSION['dataLogin']['id_cabang'] . $status;
-            $no_kontrak = generateNoKontrak($id_cabang, $lastid);
+        $no_kontrak = $this->_modelCostumer->getNoKontrak();
+        $params = $this->getRequest();
+        if ( !empty( $params[ 'params' ] ) ) {
+            $nokontrak = array_shift( $params[ 'params' ] );
+            $data = $this->_modelCostumer->getDetailPembayaran( $nokontrak );
+            $angsuran = $this->_modelPembayaran->getAngsuranKe( $nokontrak );
+            $lastAngsuran = $this->_modelPembayaran->getLastAngsuran( 0, $nokontrak );
+            $lastAngsuran = date( 'm', strtotime( $lastAngsuran[ 'tanggal_pembayaran' ] ) );
+            $last = $this->_modelPembayaran->getLastId() + 1;
+            $data[ 'no_kwitansi' ] = generateKwintansi( $last );
+            $data[ 'angsuran_ke' ] = !empty( $angsuran[ 'angsuran_ke' ] ) ? $angsuran[ 'angsuran_ke' ] + 1 : $angsuran[ 'angsuran_ke' ] + 2;
+            /* Start Cek Tanggal Terakhir Bayar */
+            if ( !empty( $lastAngsuran ) ) {
+                /* Start Cek Bulan Kemarin */
+                if ( date( 'm' ) > $lastAngsuran ) {
+                    if ( date( 'd' ) > $data[ 'tanggal_jatuh_tempo' ] ) {
+                        $totaldate = date( 'd' ) - $data[ 'tanggal_jatuh_tempo' ];
+                        $data[ 'denda' ] = $totaldate * 10000;
+                        $data[ 'potongan' ] = "00";
+
+                        $data[ 'total_bayar' ] = $data[ 'angsuran_perbulan' ] + ($totaldate * 10000);
+                    } else {
+                        $data[ 'denda' ] = '00';
+                        $data[ 'potongan' ] = "00";
+                        $data[ 'total_bayar' ] = $data[ 'angsuran_perbulan' ];
+                    }
+                }if ( date( 'm' ) <= $lastAngsuran ) {
+                    $data['bayarTerakhir'] = $lastAngsuran;
+                    $data[ 'denda' ] = '00';
+                    $data[ 'potongan' ] = 20000;
+                    $data[ 'total_bayar' ] = $data[ 'angsuran_perbulan' ] - 20000;
+                }
+            }
+            /* End Cek Tanggal Pembayaran */
+            /* Start Cek Belum Pernah Bayar */
+            #Cek Kena denda
+            elseif ( date( 'd' ) > $data[ 'tanggal_jatuh_tempo' ] ) {
+                $totaldate = date( 'd' ) - $data[ 'tanggal_jatuh_tempo' ];
+                $data[ 'denda' ] = $totaldate * 10000;
+                $data[ 'potongan' ] = "00";
+
+                $data[ 'total_bayar' ] = $data[ 'angsuran_perbulan' ] + ($totaldate * 10000);
+            }
+            #Cek Tidak Kena Denda
+            else {
+                $data[ 'denda' ] = '00';
+                $data[ 'potongan' ] = "00";
+                $data[ 'total_bayar' ] = $data[ 'angsuran_perbulan' ];
+            }
+            /* End Cek Belum Pernah Bayar */
         }
 
         require UD . 'header.html';
@@ -75,59 +114,92 @@ class IndexController extends Controller {
     }
 
     public function save() {
-        if (($_SESSION['level'] != 'superadmin') && ($_SESSION['level'] != 'kasir')) {
-            $this->redirect('error/index/notAllowed');
+        if ( ($_SESSION[ 'level' ] != 'superadmin') && ($_SESSION[ 'level' ] != 'kasir') ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
         $form = $this->getPost();
-        unset($form['nama']);
+        $form[ 'tanggal_pembayaran' ] = date( 'Y-m-d' );
+
+        $dataCetak[ 'no_kwitansi' ] = $form[ 'no_kwitansi' ];
+        $dataCetak[ 'no_kontrak' ] = $form[ 'no_kontrak' ];
+        $dataCetak[ 'nama' ] = $form[ 'nama' ];
+        $dataCetak[ 'no_polisi' ] = $form[ 'no_polisi' ];
+        $dataCetak[ 'jumlah_angsuran' ] = $form[ 'angsuran_perbulan' ];
+        $dataCetak[ 'angsuran_ke' ] = $form[ 'angsuran_ke' ];
+        $dataCetak[ 'denda' ] = $form[ 'denda' ];
+        unset( $form[ 'nama' ] );
+        unset( $form[ 'no_polisi' ] );
+
+
         try {
-            $form['tanggal_jatuh_tempo'] = date('m');
-            $this->_modelPinjaman->insert($form);
-            /* Start Update status kendaraan */
-            $where = $this->_modelKendaraan->getAdapter()->quoteInto('no_polisi = ?', $form['no_polisi']);
-            $data = array('status' => 'kredit');
-            $this->_modelKendaraan->update($data, $where);
-            /* End Update Status Kendaraan */
-            $this->redirect('pinjaman');
-        } catch (Exception $ex) {
+            $this->_modelPembayaran->insert( $form );
             require UD . 'header.html';
-            $error = $ex->getMessage();
-            $nik_costumer = $this->_modelCostumer->getNoKontrak();
-            require APP_MODUL . '/pinjaman/form/form-add-pinjaman.phtml';
+            $id_cabang = $_SESSION[ 'dataLogin' ][ 'id_cabang' ];
+            $kasirname = $this->_modelUser->getKasirName( $id_cabang );
+            $data[ 'realname' ] = $kasirname[ 'realname' ];
+            require APP_MODUL . '/pembayaran/view/kwitansi.phtml';
             require UD . 'footer.html';
+        } catch ( Exception $ex ) {
+            echo $ex->getMessage();
         }
     }
-    
+
     public function all() {
-        if (($_SESSION['level'] != 'superadmin')) {
-            $this->redirect('error/index/notAllowed');
+        if ( ($_SESSION[ 'level' ] != 'superadmin' ) ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
         require UD . 'headerDataTables.phtml';
-        $where = $this->_modelCabang->getAdapter()->quoteInto('id_cabang = ? ', $this->id_cabang);
-        $cabang = $this->_modelCabang->fetchRow($where);
-        if (!$cabang) {
-            $this->redirect('error');
+        $where = $this->_modelCabang->getAdapter()->quoteInto( 'id_cabang = ? ', $this->id_cabang );
+        $cabang = $this->_modelCabang->fetchRow( $where );
+        if ( !$cabang ) {
+            $this->redirect( 'error' );
         }
         $data = $this->_modelPinjaman->getDataPinjamanAllCabang();
-        foreach ($data as $key => $val) {
-            $no_spb = $val['no_kontrak'];
-            $where = $this->_modelSPB->getAdapter()->quoteInto('no_kontrak = ?', $no_spb);
-            $spb = $this->_modelSPB->fetchRow($where);
-            $data[$key]['spb'] = $spb->no_spb;
+        foreach ( $data as $key => $val ) {
+            $no_spb = $val[ 'no_kontrak' ];
+            $where = $this->_modelSPB->getAdapter()->quoteInto( 'no_kontrak = ?', $no_spb );
+            $spb = $this->_modelSPB->fetchRow( $where );
+            $data[ $key ][ 'spb' ] = $spb->no_spb;
         }
         require APP_MODUL . '/pinjaman/view/dataPinjamanAll.phtml';
         require UD . 'footerDataTables.phtml';
     }
-    
 
-    public function delete($id_pinjaman){
+    public function delete( $id_pinjaman ) {
         echo $id_pinjaman;
     }
-    
-    public function edit($id_pinjaman){
+
+    public function edit( $id_pinjaman ) {
         echo $id_pinjaman;
     }
-    public function detail($no_kontrak){
-        echo $no_kontrak;
+
+    public function detail( $id_pinjaman ) {
+        require UD . 'headerDataTables.phtml';
+        $where = $this->_modelCabang->getAdapter()->quoteInto( 'id_cabang = ? ', $this->id_cabang );
+        $cabang = $this->_modelCabang->fetchRow( $where );
+        if ( !$cabang ) {
+            $this->redirect( 'error' );
+        }
+        $this->id_cabang = $_SESSION[ 'dataLogin' ][ 'id_cabang' ];
+        $data = $this->_modelPembayaran->getDetailAngsuran( $id_pinjaman );
+        $detailCostumer = $this->_modelPembayaran->getLastAngsuran( $id_pinjaman, 0 );
+        $detailCostumer[ 'sisa_angsuran' ] = $detailCostumer[ 'lama_angsuran' ] - $detailCostumer[ 'angsuran_ke' ];
+        $detailCostumer[ 'sisa_pinjaman' ] = $detailCostumer[ 'nilai_pinjaman' ] - ($detailCostumer[ 'angsuran_ke' ] * $detailCostumer[ 'angsuran_perbulan' ]);
+        require APP_MODUL . '/pembayaran/view/dataDetailPembayaran.phtml';
+        require UD . 'footerDataTables.phtml';
     }
+
+    public function cetakKwitansi() {
+        $params = $this->getRequest();
+        $no_kwitansi = array_shift( $params[ 'params' ] );
+        if ( empty( $no_kwitansi ) ) {
+            $this->redirect( 'error/index/error' );
+        }
+        $dataCetak = $this->_modelPembayaran->cetakKW( $no_kwitansi );
+        $id_cabang = $_SESSION[ 'dataLogin' ][ 'id_cabang' ];
+        $kasirname = $this->_modelUser->getKasirName( $id_cabang );
+        $data[ 'realname' ] = $kasirname[ 'realname' ];
+        require APP_MODUL . '/pembayaran/view/Cetakkwitansi.phtml';
+    }
+
 }
--- application/modules/pinjaman/controller/indexcontroller.php
+++ application/modules/pinjaman/controller/indexcontroller.php
@@ -26,47 +26,47 @@ class IndexController extends Controller {
         $this->_modelSPB = Mydb::getModelSPB();
         $this->_modelUser = Mydb::getModelUser();
         $this->_modelKendaraan = Mydb::getModelKendaraan();
-        $this->id_user = $_SESSION['dataLogin']['id_user'];
-        $this->id_cabang = $_SESSION['dataLogin']['id_cabang'];
+        $this->id_user = $_SESSION[ 'dataLogin' ][ 'id_user' ];
+        $this->id_cabang = $_SESSION[ 'dataLogin' ][ 'id_cabang' ];
     }
 
     public function index() {
-        if (($_SESSION['level'] != 'superadmin') && ($_SESSION['level'] != 'admin')) {
-            $this->redirect('error/index/notAllowed');
+        if ( ($_SESSION[ 'level' ] != 'superadmin') && ($_SESSION[ 'level' ] != 'admin') ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
         require UD . 'headerDataTables.phtml';
-        $where = $this->_modelCabang->getAdapter()->quoteInto('id_cabang = ? ', $this->id_cabang);
-        $cabang = $this->_modelCabang->fetchRow($where);
-        if (!$cabang) {
-            $this->redirect('error');
+        $where = $this->_modelCabang->getAdapter()->quoteInto( 'id_cabang = ? ', $this->id_cabang );
+        $cabang = $this->_modelCabang->fetchRow( $where );
+        if ( !$cabang ) {
+            $this->redirect( 'error' );
         }
-        $data = $this->_modelPinjaman->getDataPinjamanByCabang($this->id_user, $this->id_cabang);
-        foreach ($data as $key => $val) {
-            $no_spb = $val['no_kontrak'];
-            $where = $this->_modelSPB->getAdapter()->quoteInto('no_kontrak = ?', $no_spb);
-            $spb = $this->_modelSPB->fetchRow($where);
-            $data[$key]['spb'] = (!empty($spb->no_spb)?$spb->no_spb:'');
+        $data = $this->_modelPinjaman->getDataPinjamanByCabang( $this->id_user, $this->id_cabang );
+        foreach ( $data as $key => $val ) {
+            $no_spb = $val[ 'no_kontrak' ];
+            $where = $this->_modelSPB->getAdapter()->quoteInto( 'no_kontrak = ?', $no_spb );
+            $spb = $this->_modelSPB->fetchRow( $where );
+            $data[ $key ][ 'spb' ] = (!empty( $spb->no_spb ) ? $spb->no_spb : '');
         }
         require APP_MODUL . '/pinjaman/view/dataPinjaman.phtml';
         require UD . 'footerDataTables.phtml';
     }
 
-    public function add($nopos = null) {
-        if (($_SESSION['level'] != 'superadmin') && ($_SESSION['level'] != 'admin')) {
-            $this->redirect('error/index/notAllowed');
+    public function add( $nopos = null ) {
+        if ( ($_SESSION[ 'level' ] != 'superadmin') && ($_SESSION[ 'level' ] != 'admin') ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
 
-        $nik_costumer = $this->_modelCostumer->getNoKontrak();
-        $this->id_user=$_SESSION['dataLogin']['id_user'];
-        $no_polisi = $this->_modelKendaraan->getNoPolisi($this->id_user);
-        if (!empty($nopos)) {
-            $data = $this->_modelKendaraan->getKenCostum($nopos);
-            $where = $this->_modelPinjaman->getAdapter()->quoteInto('nik_costumer =?', $data['nik_costumer']);
-            $cekData = $this->_modelPinjaman->fetchRow($where);
+        $this->id_user = $_SESSION[ 'dataLogin' ][ 'id_user' ];
+        $id_user = ($_SESSION['level']=== 'superadmin') ? null : $this->id_user;
+        $no_polisi = $this->_modelKendaraan->getNoPolisi( $id_user );
+        if ( !empty( $nopos ) ) {
+            $data = $this->_modelKendaraan->getKenCostum( $nopos );
+            $where = $this->_modelPinjaman->getAdapter()->quoteInto( 'nik_costumer =?', $data[ 'nik_costumer' ] );
+            $cekData = $this->_modelPinjaman->fetchRow( $where );
             $lastid = $this->_modelPinjaman->getLastId();
-            $status = (!empty($cekData) ? '/MD/' : '/MF/');
-            $id_cabang = $_SESSION['dataLogin']['id_cabang'] . $status;
-            $no_kontrak = generateNoKontrak($id_cabang, $lastid);
+            $status = (!empty( $cekData ) ? '-MD-' : '-MF-');
+            $id_cabang = $_SESSION[ 'dataLogin' ][ 'id_cabang' ] . $status;
+            $no_kontrak = generateNoKontrak( $id_cabang, $lastid );
         }
 
         require UD . 'header.html';
@@ -75,21 +75,21 @@ class IndexController extends Controller {
     }
 
     public function save() {
-        if (($_SESSION['level'] != 'superadmin') && ($_SESSION['level'] != 'admin')) {
-            $this->redirect('error/index/notAllowed');
+        if ( ($_SESSION[ 'level' ] != 'superadmin') && ($_SESSION[ 'level' ] != 'admin') ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
         $form = $this->getPost();
-        unset($form['nama']);
+        unset( $form[ 'nama' ] );
         try {
-            $form['tanggal_jatuh_tempo'] = date('m');
-            $this->_modelPinjaman->insert($form);
+            $form[ 'tanggal_jatuh_tempo' ] = date( 'd' );
+            $this->_modelPinjaman->insert( $form );
             /* Start Update status kendaraan */
-            $where = $this->_modelKendaraan->getAdapter()->quoteInto('no_polisi = ?', $form['no_polisi']);
-            $data = array('status' => 'kredit');
-            $this->_modelKendaraan->update($data, $where);
+            $where = $this->_modelKendaraan->getAdapter()->quoteInto( 'no_polisi = ?', $form[ 'no_polisi' ] );
+            $data = array( 'status' => 'kredit' );
+            $this->_modelKendaraan->update( $data, $where );
             /* End Update Status Kendaraan */
-            $this->redirect('pinjaman');
-        } catch (Exception $ex) {
+            $this->redirect( 'pinjaman' );
+        } catch ( Exception $ex ) {
             require UD . 'header.html';
             $error = $ex->getMessage();
             $nik_costumer = $this->_modelCostumer->getNoKontrak();
@@ -98,83 +98,83 @@ class IndexController extends Controller {
         }
     }
 
-    public function cetakSPB($id) {
-        if (($_SESSION['level'] != 'superadmin') && ($_SESSION['level'] != 'admin')) {
-            $this->redirect('error/index/notAllowed');
+    public function cetakSPB( $id ) {
+        if ( ($_SESSION[ 'level' ] != 'superadmin') && ($_SESSION[ 'level' ] != 'admin') ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
-        if (empty($id)) {
-            $this->redirect('error');
+        if ( empty( $id ) ) {
+            $this->redirect( 'error' );
         }
-        $where = $this->_modelPinjaman->getAdapterSelect()->quoteInto('id_pinjaman =?', $id);
-        $data = $this->_modelPinjaman->fetchRow($where)->toArray();
-        if (empty($data)) {
-            $this->redirect('error');
+        $where = $this->_modelPinjaman->getAdapterSelect()->quoteInto( 'id_pinjaman =?', $id );
+        $data = $this->_modelPinjaman->fetchRow( $where )->toArray();
+        if ( empty( $data ) ) {
+            $this->redirect( 'error' );
         }
-        $id_pinjaman = $data['id_pinjaman'];
-        unset($data['id_pinjaman']);
-        unset($data['nilai_pinjaman']);
-        unset($data['lama_angsuran']);
-        unset($data['angsuran_perbulan']);
-        unset($data['tanggal_jatuh_tempo']);
+        $id_pinjaman = $data[ 'id_pinjaman' ];
+        unset( $data[ 'id_pinjaman' ] );
+        unset( $data[ 'nilai_pinjaman' ] );
+        unset( $data[ 'lama_angsuran' ] );
+        unset( $data[ 'angsuran_perbulan' ] );
+        unset( $data[ 'tanggal_jatuh_tempo' ] );
         try {
-            $this->_modelSPB->insert($data);
-           
-        } catch (Exception $ex) {
+            $this->_modelSPB->insert( $data );
+        } catch ( Exception $ex ) {
             echo $ex->getMessage();
         }
         require UD . 'header.html';
-        $id_cabang = $_SESSION['dataLogin']['id_cabang'];
-        $kasirname = $this->_modelUser->getKasirName($id_cabang);
-        $data = $this->_modelPinjaman->cetakSPB($id_pinjaman);
-        $data['kepala_cabang']=$_SESSION['dataLogin']['kepala_cabang'];
-        $data['realname']=$kasirname['realname'];
+        $id_cabang = $_SESSION[ 'dataLogin' ][ 'id_cabang' ];
+        $kasirname = $this->_modelUser->getKasirName( $id_cabang );
+        $data = $this->_modelPinjaman->cetakSPB( $id_pinjaman );
+        $data[ 'kepala_cabang' ] = $_SESSION[ 'dataLogin' ][ 'kepala_cabang' ];
+        $data[ 'realname' ] = $kasirname[ 'realname' ];
         require APP_MODUL . '/pinjaman/view/spb.phtml';
         require UD . 'footer.html';
     }
-    
+
     public function all() {
-        if (($_SESSION['level'] != 'superadmin')) {
-            $this->redirect('error/index/notAllowed');
+        if ( ($_SESSION[ 'level' ] != 'superadmin' ) ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
         require UD . 'headerDataTables.phtml';
-        $where = $this->_modelCabang->getAdapter()->quoteInto('id_cabang = ? ', $this->id_cabang);
-        $cabang = $this->_modelCabang->fetchRow($where);
-        if (!$cabang) {
-            $this->redirect('error');
+        $where = $this->_modelCabang->getAdapter()->quoteInto( 'id_cabang = ? ', $this->id_cabang );
+        $cabang = $this->_modelCabang->fetchRow( $where );
+        if ( !$cabang ) {
+            $this->redirect( 'error' );
         }
         $data = $this->_modelPinjaman->getDataPinjamanAllCabang();
-        foreach ($data as $key => $val) {
-            $no_spb = $val['no_kontrak'];
-            $where = $this->_modelSPB->getAdapter()->quoteInto('no_kontrak = ?', $no_spb);
-            $spb = $this->_modelSPB->fetchRow($where);
-            $data[$key]['spb'] = $spb->no_spb;
+        foreach ( $data as $key => $val ) {
+            $no_spb = $val[ 'no_kontrak' ];
+            $where = $this->_modelSPB->getAdapter()->quoteInto( 'no_kontrak = ?', $no_spb );
+            $spb = $this->_modelSPB->fetchRow( $where );
+            $data[ $key ][ 'spb' ] = !empty( $spb->no_spb ) ? $spb->no_spb : '';
         }
         require APP_MODUL . '/pinjaman/view/dataPinjamanAll.phtml';
         require UD . 'footerDataTables.phtml';
     }
-    
-    public function spb($id_pinjaman){
-         if (($_SESSION['level'] != 'superadmin') && ($_SESSION['level'] != 'admin')) {
-            $this->redirect('error/index/notAllowed');
-         }
-         if(empty($id_pinjaman)){
-             $this->redirect('error');
-         }
-         $data = $this->_modelPinjaman->cetakSPB($id_pinjaman);
-         $id_cabang = $_SESSION['dataLogin']['id_cabang'];
-         $kasirname = $this->_modelUser->getKasirName($id_cabang);
-         $data['kepala_cabang']=$_SESSION['dataLogin']['kepala_cabang'];
-         $data['realname']=$kasirname['realname'];
+
+    public function spb( $id_pinjaman ) {
+        if ( ($_SESSION[ 'level' ] != 'superadmin') && ($_SESSION[ 'level' ] != 'admin') ) {
+            $this->redirect( 'error/index/notAllowed' );
+        }
+        if ( empty( $id_pinjaman ) ) {
+            $this->redirect( 'error' );
+        }
+        $data = $this->_modelPinjaman->cetakSPB( $id_pinjaman );
+        $id_cabang = $_SESSION[ 'dataLogin' ][ 'id_cabang' ];
+        $kasirname = $this->_modelUser->getKasirName( $id_cabang );
+        $data[ 'kepala_cabang' ] = $_SESSION[ 'dataLogin' ][ 'kepala_cabang' ];
+        $data[ 'realname' ] = $kasirname[ 'realname' ];
 
 
-         require APP_MODUL . '/pinjaman/view/spb-cetak.phtml';
+        require APP_MODUL . '/pinjaman/view/spb-cetak.phtml';
     }
-    
-    public function delete($id_pinjaman){
+
+    public function delete( $id_pinjaman ) {
         echo $id_pinjaman;
     }
-    
-    public function edit($id_pinjaman){
+
+    public function edit( $id_pinjaman ) {
         echo $id_pinjaman;
     }
+
 }
