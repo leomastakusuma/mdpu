5c60881f0e52d78eb42a03fe1fe0559b05807f24
leomastakusuma <leomastakusuma@gmail.com>
2015-08-28 22:05:17 +0700

    Update data cetak costumer

--- application/modules/costumer/controller/indexcontroller.php
+++ application/modules/costumer/controller/indexcontroller.php
@@ -21,20 +21,30 @@ class indexcontroller extends Controller {
     }
 
     public function index() {
-        if (($_SESSION['level'] != 'superadmin') && ($_SESSION['level'] != 'admin')) {
-            $this->redirect('error/index/notAllowed');
+        if ( ($_SESSION[ 'level' ] != 'superadmin') && ($_SESSION[ 'level' ] != 'admin') ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
+
+        $fields = array( 
+            array('priode'=>array('awal' => '2015-08-01','akhir' => '2015-08-30')),
+            'nik_costumer' => null,
+            'no_kontrak' => null,
+            'cabang' => 'antasari',
+            'nilai_pinjaman_min' => null,'nilai_pinjaman_max' => null,'jumlah_ang'=>2 );
+        $data = $this->_modelCostumer->geCetakksostumer(1, $fields);
+        
+
         require UD . 'headerDataTables.phtml';
-        $cabang = $_SESSION['dataLogin']['id_cabang'];
-        $data = $this->_modelCostumer->getAllCostumerByCabang($cabang,null);
-        $cabang=$_SESSION['dataLogin']['cabang'];
+        $cabang = $_SESSION[ 'dataLogin' ][ 'id_cabang' ];
+        $data = $this->_modelCostumer->getAllCostumerByCabang( $cabang, null );
+        $cabang = $_SESSION[ 'dataLogin' ][ 'cabang' ];
         require APP_MODUL . '/costumer/view/dataCostumer.phtml';
         require UD . 'footerDataTables.phtml';
     }
 
     public function all() {
-        if (($_SESSION['level'] != 'superadmin')) {
-            $this->redirect('error/index/notAllowed');
+        if ( ($_SESSION[ 'level' ] != 'superadmin' ) ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
 
         require UD . 'headerDataTables.phtml';
@@ -44,8 +54,8 @@ class indexcontroller extends Controller {
     }
 
     public function add() {
-        if (($_SESSION['level'] != 'superadmin') && ($_SESSION['level'] != 'admin')) {
-            $this->redirect('error/index/notAllowed');
+        if ( ($_SESSION[ 'level' ] != 'superadmin') && ($_SESSION[ 'level' ] != 'admin') ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
         require UD . 'header.html';
         require APP_MODUL . '/costumer/form/form-add-costumer.phtml';
@@ -53,16 +63,16 @@ class indexcontroller extends Controller {
     }
 
     public function save() {
-        if (($_SESSION['level'] != 'superadmin') && ($_SESSION['level'] != 'admin')) {
-            $this->redirect('error/index/notAllowed');
+        if ( ($_SESSION[ 'level' ] != 'superadmin') && ($_SESSION[ 'level' ] != 'admin') ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
         $form = $this->getPost();
-        $form['id_user'] = $_SESSION['dataLogin']['id_user'];
-        $form['id_cabang'] = $_SESSION['dataLogin']['id_cabang'];
+        $form[ 'id_user' ] = $_SESSION[ 'dataLogin' ][ 'id_user' ];
+        $form[ 'id_cabang' ] = $_SESSION[ 'dataLogin' ][ 'id_cabang' ];
         try {
-            $this->_modelCostumer->insert($form);
-            $this->redirect('costumer');
-        } catch (Exception $ex) {
+            $this->_modelCostumer->insert( $form );
+            $this->redirect( 'costumer' );
+        } catch ( Exception $ex ) {
             require UD . 'header.html';
             $error = $ex->getMessage();
             require APP_MODUL . '/costumer/form/form-add-costumer.phtml';
@@ -70,53 +80,53 @@ class indexcontroller extends Controller {
         }
     }
 
-    public function detail($id) {
-        if (($_SESSION['level'] != 'superadmin') && ($_SESSION['level'] != 'admin')) {
-            $this->redirect('error/index/notAllowed');
+    public function detail( $id ) {
+        if ( ($_SESSION[ 'level' ] != 'superadmin') && ($_SESSION[ 'level' ] != 'admin') ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
-        if (!isset($id)) {
-            $this->redirect('error');
+        if ( !isset( $id ) ) {
+            $this->redirect( 'error' );
         }
-        $data = $this->_modelCostumer->getCostumer($id);
-        $cekPenjamin = $this->_modelCostumer->getPenjaminCostumer($id);
-        $whereKendaraan = $this->_modelKendaraan->getAdapter()->quoteInto('nik_costumer = ?', $id);
-        $cekKendaraan = $this->_modelKendaraan->fetchRow($whereKendaraan);
-        $wherePinjaman = $this->_modelPinjaman->getAdapter()->quoteInto('nik_costumer = ?', $id);
-        $cekPinjaman = $this->_modelPinjaman->fetchRow($wherePinjaman);
-        
+        $data = $this->_modelCostumer->getCostumer( $id );
+        $cekPenjamin = $this->_modelCostumer->getPenjaminCostumer( $id );
+        $whereKendaraan = $this->_modelKendaraan->getAdapter()->quoteInto( 'nik_costumer = ?', $id );
+        $cekKendaraan = $this->_modelKendaraan->fetchRow( $whereKendaraan );
+        $wherePinjaman = $this->_modelPinjaman->getAdapter()->quoteInto( 'nik_costumer = ?', $id );
+        $cekPinjaman = $this->_modelPinjaman->fetchRow( $wherePinjaman );
+
         require UD . 'header.html';
         require APP_MODUL . '/costumer/form/form-detail-costumer.phtml';
         require UD . 'footer.html';
     }
-    
-     public function delete($id) {
-        if ($_SESSION['level'] != 'superadmin') {
-            $this->redirect('error/index/notAllowed');
+
+    public function delete( $id ) {
+        if ( $_SESSION[ 'level' ] != 'superadmin' ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
-        if (!isset($id)) {
-            $this->redirect('error');
+        if ( !isset( $id ) ) {
+            $this->redirect( 'error' );
         }
-        $where = $this->_modelCostumer->getAdapter()->quoteInto('nik_costumer = ?', $id);
+        $where = $this->_modelCostumer->getAdapter()->quoteInto( 'nik_costumer = ?', $id );
         try {
-            $this->_modelCostumer->delete($where);
-            $this->redirect('costumer');
-        } catch (Exception $ex) {
+            $this->_modelCostumer->delete( $where );
+            $this->redirect( 'costumer' );
+        } catch ( Exception $ex ) {
             echo $ex->getMessage();
         }
     }
 
-    public function edit($id) {
-        if ($_SESSION['level'] != 'superadmin') {
-            $this->redirect('error/index/notAllowed');
+    public function edit( $id ) {
+        if ( $_SESSION[ 'level' ] != 'superadmin' ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
-        if (!isset($id)) {
-            $this->redirect('error');
+        if ( !isset( $id ) ) {
+            $this->redirect( 'error' );
         }
-        $where = $this->_modelCostumer->getAdapter()->quoteInto('nik_costumer = ?', $id);
-        $data = $this->_modelCostumer->fetchRow($where);
-        
-        if (empty($data)) {
-            $this->redirect('error');
+        $where = $this->_modelCostumer->getAdapter()->quoteInto( 'nik_costumer = ?', $id );
+        $data = $this->_modelCostumer->fetchRow( $where );
+
+        if ( empty( $data ) ) {
+            $this->redirect( 'error' );
         }
         $config = Mydb::getConfig();
         $agama = $config->agama;
@@ -124,22 +134,24 @@ class indexcontroller extends Controller {
         require APP_MODUL . '/costumer/form/form-edit-costumer.phtml';
         require UD . 'footer.html';
     }
-    public function cetakCostumer(){
-		require UD . 'header.html';
+
+    public function cetakCostumer() {
+        require UD . 'header.html';
         $data = $this->_modelCostumer->getCetakKostumer();
-		require APP_MODUL . '/costumer/view/cetakCostumer.phtml';
+        require APP_MODUL . '/costumer/view/cetakCostumer.phtml';
         require UD . 'footer.html';
-	} 
+    }
+
     public function saveedit() {
-        if ($_SESSION['level'] != 'superadmin') {
-            $this->redirect('error/index/notAllowed');
+        if ( $_SESSION[ 'level' ] != 'superadmin' ) {
+            $this->redirect( 'error/index/notAllowed' );
         }
         $form = $this->getPost();
-        $where = $this->_modelCostumer->getAdapter()->quoteInto('id_costumer = ?', $form['id_costumer']);
+        $where = $this->_modelCostumer->getAdapter()->quoteInto( 'id_costumer = ?', $form[ 'id_costumer' ] );
         try {
-            $this->_modelCostumer->update($form, $where);
-            $this->redirect('costumer');
-        } catch (Exception $ex) {
+            $this->_modelCostumer->update( $form, $where );
+            $this->redirect( 'costumer' );
+        } catch ( Exception $ex ) {
             $config = Mydb::getConfig();
             $agama = $config->agama;
             require UD . 'header.html';
@@ -148,4 +160,5 @@ class indexcontroller extends Controller {
             require UD . 'footer.html';
         }
     }
+
 }
--- library/Mydb/Db/Costumer.php
+++ library/Mydb/Db/Costumer.php
@@ -8,169 +8,231 @@
  * @package    Mydb_Db
  * @subpackage Mydb_Db_Costumer
  */
+class Mydb_Db_Costumer extends Mydb_Db_Abstract {
 
-class Mydb_Db_Costumer extends Mydb_Db_Abstract{
-    
     protected $_name = 'costumer';
     protected $_primary = 'nik_costumer';
-    
-    public function getAllCostumerByCabang($cabang = null,$id=null){
+
+    public function getAllCostumerByCabang( $cabang = null, $id = null ) {
         $select = $this->select();
-        $select->from(array('cos'=>$this->_name),array());
-        $select->setIntegrityCheck(false);
-        $select->columns(array('nik_costumer'),'cos');
-        $select->columns(array('nama'),'cos');
-        $select->columns(array('alamat'),'cos');
-        $select->columns(array('jenis_kelamin'),'cos');
-        $select->columns(array('tempat_lahir'),'cos');
-        $select->columns(array('tanggal_lahir'),'cos');
-        $select->columns(array('nama_ibu'),'cos');
-        $select->columns(array('agama'),'cos');
-        $select->columns(array('status'),'cos');
-        $select->columns(array('pekerjaan'),'cos');
-        $select->columns(array('alamat_tempat_kerja'),'cos');
-        $select->columns(array('hp'),'cos');
-        $select->columns(array('telpon'),'cos');      
-        
-        $select->join(array('us'=>'user'), 'cos.id_user=us.id_user',array('realname'));
-        $select->join(array('cb'=>'cabang'), 'us.id_cabang = cb.id_cabang',array());
-        if(!empty($id)){
-            $select->where('cos.id_user = ?',$id);
-        }if(!empty($cabang)){
-            $select->where('cos.id_cabang = ?',$cabang);
+        $select->from( array( 'cos' => $this->_name ), array() );
+        $select->setIntegrityCheck( false );
+        $select->columns( array( 'nik_costumer' ), 'cos' );
+        $select->columns( array( 'nama' ), 'cos' );
+        $select->columns( array( 'alamat' ), 'cos' );
+        $select->columns( array( 'jenis_kelamin' ), 'cos' );
+        $select->columns( array( 'tempat_lahir' ), 'cos' );
+        $select->columns( array( 'tanggal_lahir' ), 'cos' );
+        $select->columns( array( 'nama_ibu' ), 'cos' );
+        $select->columns( array( 'agama' ), 'cos' );
+        $select->columns( array( 'status' ), 'cos' );
+        $select->columns( array( 'pekerjaan' ), 'cos' );
+        $select->columns( array( 'alamat_tempat_kerja' ), 'cos' );
+        $select->columns( array( 'hp' ), 'cos' );
+        $select->columns( array( 'telpon' ), 'cos' );
+
+        $select->join( array( 'us' => 'user' ), 'cos.id_user=us.id_user', array( 'realname' ) );
+        $select->join( array( 'cb' => 'cabang' ), 'us.id_cabang = cb.id_cabang', array() );
+        if ( !empty( $id ) ) {
+            $select->where( 'cos.id_user = ?', $id );
+        }if ( !empty( $cabang ) ) {
+            $select->where( 'cos.id_cabang = ?', $cabang );
         }
-               
-        return $this->getAdapter()->fetchAll($select);
-       
+
+        return $this->getAdapter()->fetchAll( $select );
     }
-	
-	public function getCetakKostumer(){
-	     $select = $this->select();
-		 $select->from($this->_name,array('*'));
-		 return $this->getAdapter()->fetchAll($select);
-	
-	}
-	
-	
-    public function getAllCostumer(){
+
+    public function getCetakKostumer() {
         $select = $this->select();
-        $select->from(array('cos'=>$this->_name),array());
-        $select->setIntegrityCheck(false);
-        $select->columns(array('id_costumer'),'cos');
-        $select->columns(array('nik_costumer'),'cos');
-        $select->columns(array('nama'),'cos');
-        $select->columns(array('alamat'),'cos');
-        $select->columns(array('jenis_kelamin'),'cos');
-        $select->columns(array('tempat_lahir'),'cos');
-        $select->columns(array('tanggal_lahir'),'cos');
-        $select->columns(array('nama_ibu'),'cos');
-        $select->columns(array('agama'),'cos');
-        $select->columns(array('status'),'cos');
-        $select->columns(array('pekerjaan'),'cos');
-        $select->columns(array('alamat_tempat_kerja'),'cos');
-        $select->columns(array('hp'),'cos');
-        $select->columns(array('telpon'),'cos');      
-        
-        $select->join(array('us'=>'user'), 'cos.id_user=us.id_user',array('realname'));
-        $select->join(array('cb'=>'cabang'), 'cb.id_cabang = cos.id_cabang',array('cabang'));
-        
-        return $this->getAdapter()->fetchAll($select);
-       
+        $select->from( $this->_name, array( '*' ) );
+        return $this->getAdapter()->fetchAll( $select );
     }
-    
-    public function getCostumer($id){
+
+    public function getAllCostumer() {
         $select = $this->select();
-        $select->from(array('cos'=>$this->_name),array());
-        $select->setIntegrityCheck(false);
-        $select->columns(array('nik_costumer'),'cos');
-        $select->columns(array('nama'),'cos');
-        $select->columns(array('alamat'),'cos');
-        $select->columns(array('jenis_kelamin'),'cos');
-        $select->columns(array('tempat_lahir'),'cos');
-        $select->columns(array('tanggal_lahir'),'cos');
-        $select->columns(array('nama_ibu'),'cos');
-        $select->columns(array('agama'),'cos');
-        $select->columns(array('status'),'cos');
-        $select->columns(array('pekerjaan'),'cos');
-        $select->columns(array('alamat_tempat_kerja'),'cos');
-        $select->columns(array('hp'),'cos');
-        $select->columns(array('telpon'),'cos');      
-        $select->columns(array('npwp'),'cos');
-	$select->columns(array('kota'),'cos');
-	$select->columns(array('provinsi'),'cos');
-	$select->columns(array('kode_pos'),'cos');
-	$select->columns(array('penghasilan_perbulan'),'cos');
-	$select->columns(array('jumlah_tanggungan'),'cos');
-        $select->join(array('us'=>'user'), 'cos.id_user=us.id_user',array('realname'));
-        $select->join(array('cb'=>'cabang'), 'us.id_cabang = cb.id_cabang',array('cabang'));
-        
-        $select->where('cos.nik_costumer = ?',$id);
-        return $this->getAdapter()->fetchRow($select);
+        $select->from( array( 'cos' => $this->_name ), array() );
+        $select->setIntegrityCheck( false );
+        $select->columns( array( 'id_costumer' ), 'cos' );
+        $select->columns( array( 'nik_costumer' ), 'cos' );
+        $select->columns( array( 'nama' ), 'cos' );
+        $select->columns( array( 'alamat' ), 'cos' );
+        $select->columns( array( 'jenis_kelamin' ), 'cos' );
+        $select->columns( array( 'tempat_lahir' ), 'cos' );
+        $select->columns( array( 'tanggal_lahir' ), 'cos' );
+        $select->columns( array( 'nama_ibu' ), 'cos' );
+        $select->columns( array( 'agama' ), 'cos' );
+        $select->columns( array( 'status' ), 'cos' );
+        $select->columns( array( 'pekerjaan' ), 'cos' );
+        $select->columns( array( 'alamat_tempat_kerja' ), 'cos' );
+        $select->columns( array( 'hp' ), 'cos' );
+        $select->columns( array( 'telpon' ), 'cos' );
+
+        $select->join( array( 'us' => 'user' ), 'cos.id_user=us.id_user', array( 'realname' ) );
+        $select->join( array( 'cb' => 'cabang' ), 'cb.id_cabang = cos.id_cabang', array( 'cabang' ) );
+
+        return $this->getAdapter()->fetchAll( $select );
     }
+
+    public function getCostumer( $id ) {
+        $select = $this->select();
+        $select->from( array( 'cos' => $this->_name ), array() );
+        $select->setIntegrityCheck( false );
+        $select->columns( array( 'nik_costumer' ), 'cos' );
+        $select->columns( array( 'nama' ), 'cos' );
+        $select->columns( array( 'alamat' ), 'cos' );
+        $select->columns( array( 'jenis_kelamin' ), 'cos' );
+        $select->columns( array( 'tempat_lahir' ), 'cos' );
+        $select->columns( array( 'tanggal_lahir' ), 'cos' );
+        $select->columns( array( 'nama_ibu' ), 'cos' );
+        $select->columns( array( 'agama' ), 'cos' );
+        $select->columns( array( 'status' ), 'cos' );
+        $select->columns( array( 'pekerjaan' ), 'cos' );
+        $select->columns( array( 'alamat_tempat_kerja' ), 'cos' );
+        $select->columns( array( 'hp' ), 'cos' );
+        $select->columns( array( 'telpon' ), 'cos' );
+        $select->columns( array( 'npwp' ), 'cos' );
+        $select->columns( array( 'kota' ), 'cos' );
+        $select->columns( array( 'provinsi' ), 'cos' );
+        $select->columns( array( 'kode_pos' ), 'cos' );
+        $select->columns( array( 'penghasilan_perbulan' ), 'cos' );
+        $select->columns( array( 'jumlah_tanggungan' ), 'cos' );
+        $select->join( array( 'us' => 'user' ), 'cos.id_user=us.id_user', array( 'realname' ) );
+        $select->join( array( 'cb' => 'cabang' ), 'us.id_cabang = cb.id_cabang', array( 'cabang' ) );
+
+        $select->where( 'cos.nik_costumer = ?', $id );
+        return $this->getAdapter()->fetchRow( $select );
+    }
+
     /**
      * 
      * @param type $nik_costumer
      */
-    public function getPenjaminCostumer($nik_costumer){
+    public function getPenjaminCostumer( $nik_costumer ) {
         $select = $this->select();
-        $select->from(array('cos'=>$this->_name),array());
-        $select->setIntegrityCheck(false);
-        $select->join(array('penj'=>'penjamin'),'penj.nik_costumer = cos.nik_costumer',array('*'));
-        $select->where('cos.nik_costumer = ?',$nik_costumer);
-        return $this->getAdapterSelect()->fetchAll($select);
-        
-        
+        $select->from( array( 'cos' => $this->_name ), array() );
+        $select->setIntegrityCheck( false );
+        $select->join( array( 'penj' => 'penjamin' ), 'penj.nik_costumer = cos.nik_costumer', array( '*' ) );
+        $select->where( 'cos.nik_costumer = ?', $nik_costumer );
+        return $this->getAdapterSelect()->fetchAll( $select );
     }
-    
-   /* public function getNoKontrak(){
+
+    /* public function getNoKontrak(){
+      $select = $this->select();
+      $select->from($this->_name,array('nik_costumer'));
+      return $this->getAdapterSelect()->fetchAll($select);
+      }
+     */
+
+    public function getNoKontrak( $laporan = false ) {
         $select = $this->select();
-        $select->from($this->_name,array('nik_costumer'));
-        return $this->getAdapterSelect()->fetchAll($select);
-    }
-    */
-    
-    public function getNoKontrak($laporan = false){
-        $select = $this->select();
-        $select->from(array('cos'=>$this->_name),array());
-        $select->setIntegrityCheck(false);
-        $select->join(array('pinj'=>'pinjaman'), 'pinj.nik_costumer =  cos.nik_costumer',array());
-        $select->join(array('kend'=>'kendaraan'),'kend.no_polisi = pinj.no_polisi',array());
-        if($laporan){
-            $select->join(array('pem'=>'pembayaran'),'pinj.no_kontrak = pem.no_kontrak',array());
+        $select->from( array( 'cos' => $this->_name ), array() );
+        $select->setIntegrityCheck( false );
+        $select->join( array( 'pinj' => 'pinjaman' ), 'pinj.nik_costumer =  cos.nik_costumer', array() );
+        $select->join( array( 'kend' => 'kendaraan' ), 'kend.no_polisi = pinj.no_polisi', array() );
+        if ( $laporan ) {
+            $select->join( array( 'pem' => 'pembayaran' ), 'pinj.no_kontrak = pem.no_kontrak', array() );
         }
-        $select->columns(array('no_kontrak'),'pinj');
-        $select->columns(array('lama_angsuran'),'pinj');
-		$select->group('pinj.no_kontrak');
-        return $this->getAdapterSelect()->fetchAll($select);
+        $select->columns( array( 'no_kontrak' ), 'pinj' );
+        $select->columns( array( 'lama_angsuran' ), 'pinj' );
+        $select->group( 'pinj.no_kontrak' );
+        return $this->getAdapterSelect()->fetchAll( $select );
     }
-    
-    public function getDetailPembayaran($no_kontrak){
+
+    public function getDetailPembayaran( $no_kontrak ) {
         $select = $this->select();
-        $select->from(array('cos'=>$this->_name),array());
-        $select->setIntegrityCheck(false);
-        $select->join(array('pinj'=>'pinjaman'), 'pinj.nik_costumer =  cos.nik_costumer',array());
-        $select->join(array('kend'=>'kendaraan'),'kend.no_polisi = pinj.no_polisi',array());
-        $select->join(array('cab'=>'cabang'),'cab.id_cabang = cos.id_cabang',array('cabang'));
-        $select->columns(array('no_kontrak'),'pinj');
-        $select->columns(array('nama'),'cos');
-        $select->columns(array('no_polisi'),'kend');
-        $select->columns(array('angsuran_perbulan'),'pinj');
-        $select->columns(array('tanggal_jatuh_tempo'),'pinj');
-        $select->where('pinj.no_kontrak = ?',$no_kontrak);
-        $select->group('pinj.no_kontrak');
-        return $this->getAdapterSelect()->fetchRow($select);
+        $select->from( array( 'cos' => $this->_name ), array() );
+        $select->setIntegrityCheck( false );
+        $select->join( array( 'pinj' => 'pinjaman' ), 'pinj.nik_costumer =  cos.nik_costumer', array() );
+        $select->join( array( 'kend' => 'kendaraan' ), 'kend.no_polisi = pinj.no_polisi', array() );
+        $select->join( array( 'cab' => 'cabang' ), 'cab.id_cabang = cos.id_cabang', array( 'cabang' ) );
+        $select->columns( array( 'no_kontrak' ), 'pinj' );
+        $select->columns( array( 'nama' ), 'cos' );
+        $select->columns( array( 'no_polisi' ), 'kend' );
+        $select->columns( array( 'angsuran_perbulan' ), 'pinj' );
+        $select->columns( array( 'tanggal_jatuh_tempo' ), 'pinj' );
+        $select->where( 'pinj.no_kontrak = ?', $no_kontrak );
+        $select->group( 'pinj.no_kontrak' );
+        return $this->getAdapterSelect()->fetchRow( $select );
     }
-    
-    public function getCabangCostumer($no_kontrak){
+
+    public function getCabangCostumer( $no_kontrak ) {
         $select = $this->select();
-        $select->from(array('cos'=>$this->_name),array());
-        $select->setIntegrityCheck(false);
-        $select->join(array('pinj'=>'pinjaman'), 'pinj.nik_costumer =  cos.nik_costumer',array());
-        $select->join(array('cab'=>'cabang'),'cos.id_cabang = cab.id_cabang');
-        $select->columns(array('id_cabang'),'cab');
-        $select->columns(array('cabang'),'cab');
-        $select->where('pinj.no_kontrak = ?',$no_kontrak);
-        return $this->getAdapterSelect()->fetchRow($select);
+        $select->from( array( 'cos' => $this->_name ), array() );
+        $select->setIntegrityCheck( false );
+        $select->join( array( 'pinj' => 'pinjaman' ), 'pinj.nik_costumer =  cos.nik_costumer', array() );
+        $select->join( array( 'cab' => 'cabang' ), 'cos.id_cabang = cab.id_cabang' );
+        $select->columns( array( 'id_cabang' ), 'cab' );
+        $select->columns( array( 'cabang' ), 'cab' );
+        $select->where( 'pinj.no_kontrak = ?', $no_kontrak );
+        return $this->getAdapterSelect()->fetchRow( $select );
+    }
+
+    public function geCetakksostumer( $cabang = false, $fields = array() ) {
+        $select = $this->select();
+        $select->from( array( 'cos' => 'costumer' ), array() );
+        $select->setIntegrityCheck( false );
+        $select->join( array( 'penj' => 'penjamin' ), 'penj.nik_costumer = cos.nik_costumer', array() );
+        $select->join( array( 'ken' => 'kendaraan' ), 'ken.nik_costumer = cos.nik_costumer', array() );
+        $select->join( array( 'pinj' => 'pinjaman' ), 'pinj.nik_costumer = cos.nik_costumer', array() );
+        $select->joinLeft( array( 'cab' => 'cabang' ), 'cab.id_cabang=cos.id_cabang', array() );
+        $select->columns( array( 'nik_costumer' ), 'cos' );
+        $select->columns( array( 'nama' ), 'cos' );
+        $select->columns( array( 'alamat' ), 'cos' );
+        $select->columns( array( 'nik_penjamin' ), 'penj' );
+        $select->columns( array( 'nama' ), 'penj' );
+        $select->columns( array( 'no_polisi' ), 'ken' );
+        $select->columns( array( 'merk' ), 'ken' );
+        $select->columns( array( 'no_kontrak' ), 'pinj' );
+        $select->columns( array( 'nilai_pinjaman' ), 'pinj' );
+        $select->columns( array( 'angsuran_perbulan' ), 'pinj' );
+        $select->columns( array( 'lama_angsuran' ), 'pinj' );
+        $select->joinLeft( array( 'bb_kas' => 'bb_penerimaan_kas' ), 'bb_kas.no_kontrak =pinj.no_kontrak', array( new Zend_Db_Expr( 'count(bb_kas.no_kontrak) as total_ang' ) ) );
+        $select->columns( array( 'cabang' ), 'cab' );
+
+        /* Kondisi Inputan */
+        if ( !empty( $fields ) ) {
+            foreach ( $fields as $key => $row ) {
+                if ( !empty( $row ) ) {
+                    if ( !empty( $row[ 'priode' ] ) ) {
+                        foreach ( $row[ 'priode' ] as $k => $v ) {
+                            if ( !empty( $k === 'awal' ) ) {
+                                $select->where( 'pinj.tanggal >= ? ', $v );
+                            }if ( !empty( $k === 'akhir' ) ) {
+                                $select->where( 'pinj.tanggal <= ? ', $v );
+                            }
+                        }
+                    }if ( !empty( $key === 'nik_costumer' ) && !empty( $row ) ) {
+                        $select->where( 'cos.nik_costumer = ? ', $row );
+                    }if ( !empty( $key === 'no_kontrak' ) && !empty( $row ) ) {
+                        $select->where( 'pinj.no_kontrak= ? ', $row );
+                    }if ( !empty( $key === 'nilai_pinjaman_min' ) ) {
+                        $select->where( 'pinj.nilai_pinjaman >= ?', $row );
+                    }if ( !empty( $key === 'nilai_pinjaman_max' ) ) {
+                        $select->where( 'pinj.nilai_pinjaman <= ?', $row );
+                    }if ( !empty( $key === 'jumlah_ang' ) ) {
+                        $select->where( 'pinj.lama_angsuran = ?', $row );
+                    }
+                }
+            }
+        }
+
+        if ( $cabang ) {
+            $select->where( 'cos.id_cabang = ?',$cabang );
+        }
+
+        /* End Kondisi */
+
+        $select->order( 'cos.nik_costumer' );
+        $select->group( 'cos.nik_costumer' );
+
+
+
+
+        echo $select;
+
+
+
+        return;
     }
 
 }
